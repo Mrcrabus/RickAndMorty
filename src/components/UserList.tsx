0707060ad62a56ce35ca4db4e61b4cb371e3954a
IgorNull <igor.bodenchuk@innowise-group.com>
2023-01-09 16:44:10 +0300

    Feature(): infinity scroll

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -3,82 +3,89 @@ import {useTypedSelector} from "../hooks/useTypedSelector";
 import {fetchUsers} from "../store/action-creators/user";
 import {useActions} from "../hooks/useActions";
 import {Avatar, Card, Container, Grid, Pagination, Typography, CardActionArea} from '@mui/material';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const UserList: FC = () => {
-  const {users, loading, error, page, limit} = useTypedSelector(state => state.users)
+  const {users, error, page, limit} = useTypedSelector(state => state.users)
   const {fetchUsers, setUsersPage} = useActions()
 
 
   useEffect(() => {
     fetchUsers(page, limit)
+
   }, [page])
 
-  if (loading) {
-    return <h1>Loading...</h1>
-  }
   if (error) {
     return <h1>{error}</h1>
   }
 
-  const handleChange = (e: any, p: number) => {
-    setUsersPage(p);
+
+  const handleChange = () => {
+    setUsersPage(page + 1);
+    console.log(123)
   };
 
 
   return (
     <Container>
-
-      <Grid
-        sx={{
-          flexGrow: 1,
-          display: 'flex',
-          justifyContent: 'center',
-          gap: 6,
-          flexWrap: 'wrap',
-          '& > *': {minWidth: 0, flexBasis: 200},
-        }}
+      <InfiniteScroll
+        next={handleChange}
+        hasMore={true}
+        loader={<h1>Loading...</h1>}
+        dataLength={826}
       >
-        {users.map(user =>
-          <CardActionArea>
-            <Card
-              key={user.id}
-              variant="outlined"
-              sx={{
-                maxWidth: 345,
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                paddingTop: 1
-              }}
+        <Grid
+          sx={{
+            flexGrow: 1,
+            display: 'flex',
+            justifyContent: 'center',
+            gap: 6,
+            flexWrap: 'wrap',
+            '& > *': {minWidth: 0, flexBasis: 200},
+          }}
+        >
+          {users.map(user =>
+            <CardActionArea>
+              <Card
+                key={user.id}
+                variant="outlined"
+                sx={{
+                  maxWidth: 345,
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  padding: 10
+                }}
 
-            >
+              >
 
-              <Avatar
-                alt={user.image}
-                src={user.image}
-                sx={{width: 50, height: 50}}
-              />
-              <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
-                {user.name}
-              </Typography>
+                <Avatar
+                  alt={user.image}
+                  src={user.image}
+                  sx={{width: 50, height: 50}}
+                />
+                <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                  {user.name}
+                </Typography>
 
-            </Card>
-          </CardActionArea>
-        )}
-      </Grid>
+              </Card>
+            </CardActionArea>
+          )}
+        </Grid>
+      </InfiniteScroll>
 
 
-      <Pagination
-        count={42}
-        page={page}
-        onChange={handleChange}
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: 10
-        }}
-      />
+      {/*<Pagination*/}
+      {/*  count={42}*/}
+      {/*  page={page}*/}
+      {/*  onChange={handleChange}*/}
+      {/*  sx={{*/}
+      {/*    display: "flex",*/}
+      {/*    justifyContent: "center",*/}
+      {/*    marginTop: 10*/}
+      {/*  }}*/}
+      {/*/>*/}
 
 
     </Container>
--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -6,19 +6,19 @@ const initialState: IUserState = {
   error: null,
   page: 1,
   limit: 20
-} 
+}
 
 
-export const userReducer = (state = initialState, action: UserAction): IUserState  => {
+export const userReducer = (state = initialState, action: UserAction): IUserState => {
   switch (action.type) {
     case UserActionTypes.FETCH_USERS :
       return {...state, loading: true}
     case UserActionTypes.FETCH_USERS_SUCCESS :
-      return {...state, loading: false, users: action.payload}
+      return {...state, loading: false, users: [...state.users, ...action.payload]}
     case UserActionTypes.FETCH_USERS_ERROR :
-      return {... state, loading: false, error: action.payload, users: []}
+      return {...state, loading: false, error: action.payload, users: []}
     case UserActionTypes.SET_USERS_PAGE:
-      return {...state, page: action.payload}
+      return {...state, page:  action.payload}
     default:
       return state
   }
